2ad1299949049a74870216aab3b1995d9f792a2b
jaymz95 <jamesmullarkey15@gmail.com>
2019-11-11 18:04:37 +0000

    flask web app displaying html file

---
+++ views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+#from .models import Comment
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    #comments = Comment.query.all()
+    return  render_template('index.html')
+    # '<h2>Hello</h2>'
