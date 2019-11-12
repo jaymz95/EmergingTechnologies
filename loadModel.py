74830cb21c9d52e09af3fa2af75e24f9371311fc
jaymz95 <jamesmullarkey15@gmail.com>
2019-11-12 18:56:23 +0000

    Working on getting my button to send drawn image

---
+++ loadModel.py
@@ -0,0 +1,4 @@
+import numpy as np
+import keras.models
+from keras.models import model_from_json
+
--- views.py
+++ views.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, render_template
 #from flask_bootstrap import Bootstrap
-#from .models import Comment
 
 main = Blueprint('main', __name__)
 
-
 @main.route('/')
 def index():
-    #comments = Comment.query.all()
     return  render_template('index.html')
-    # '<h2>Hello</h2>'
+
