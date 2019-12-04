f3333a1e0fc0e1068969f75caa4c94e20e00b164
jaymz95 <jamesmullarkey15@gmail.com>
2019-12-04 20:24:07 +0000

    Code Tidied up and references made in README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # EmergingTechnologies
+
+
+# References for flask
+#### References, research and documentation for Jupyter Notebook are in the Jupyter Notbook
+
+- https://www.html5canvastutorials.com/labs/html5-canvas-paint-application/
+- https://scikit-image.org/docs/dev/auto_examples/transform/plot_rescale.html
+- https://docs.scipy.org/doc/numpy/reference/generated/numpy.transpose.html
+- https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+- https://www.youtube.com/watch?time_continue=1193&v=wQ8BIBpya2k
+- https://www.youtube.com/watch?v=f6Bf3gl4hWY&t=1510s
+- http://tutorials.jenkov.com/html5-canvas/clearrect.html
+- https://github.com/keras-team/keras/issues/8661
+- https://www.geeksforgeeks.org/numpy-reshape-python/
+- https://www.journaldev.com/32984/numpy-matrix-transpose-array
+- https://docs.scipy.org/doc/numpy/reference/generated/numpy.reshape.html#numpy.reshape
+- https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/disable_v2_behavior?hl=ca
+- https://stackoverflow.com/questions/57968999/runtimeerror-attempting-to-capture-an-eagertensor-without-building-a-function
+- https://stackoverflow.com/questions/48270857/python-takes-1-positional-argument-but-2-were-given
--- views.py
+++ views.py
@@ -30,11 +30,7 @@ from tensorflow.keras import backend as be
 import matplotlib.pyplot as plt
 
 def convertImage(imgData1):
-    #("yesssss: ")
-    #print(imgData1)
     imgstr = re.search(b'base64,(.*)',imgData1).group(1)
-    #print("Hello: ")
-    #print(imgstr)
     with open('output.png','wb') as output:
         output.write(base64.b64decode(imgstr))
 
@@ -47,56 +43,24 @@ def index():
 @main.route('/predict', methods=['GET', 'POST'])
 def predict():
     imgData = request.get_data()
-    #print(imgData.ndim)
-    #print(imgData.shape)
-    
-    
     convertImage(imgData)
     x = imread('output.png')
     
-    print(x.ndim)
-    print(x.shape)
+    # Gets the last layer on the canvas becasue it returns 3D array
+    # this gets the final element in the array
     x = np.transpose(x, axes=[2, 0, 1])
     x = x[3]
-    
-    #x = x.convert('1')
-    #for i in range(x.shape[0]):
-    #    print(x[i].shape)
-    #    x = x[i]
 
-    #x1 = x[0]
-    #plt.imshow(x, cmap= plt.cm.binary)
-    #plt.show()
-    #plt.imshow(x[1])
-    #plt.show()
     x = np.invert(x)
-    print(x.ndim)
-    print(x.shape)
     x = resize(x, (28, 28))
-    #x = rescale(x, 0.5).shape
-
-    #x = x.transpose(2,0,1).reshape(3,-1)
-    #cv2.imshow("hiya", x)
-    #x = x.reshape(1, 28, 28, 1)
-    print("TEST")
     
     print(x.ndim)
     print(x.shape)
     #x = x.transpose(2,0,1).reshape(-1,x.shape[1])
-    print(x.ndim)
-    print(x.shape)
-    print("TEST2")
-
-    #x = cv2.resize(x, dsize=(28, 28))
     x = resize(x, (28, 28))
     #plt.imshow(x)
     #plt.show()
-    #x = resize(x, (28, 28))
-    print("what: ")
     print(len(x))
-    
-    #plt.imshow(x)
-    #plt.show()
     x = x.reshape(1, 28, 28, 1)
     
     print(x.ndim)
@@ -106,22 +70,8 @@ def predict():
     global model, graph
     model, graph = init()
     model._make_predict_function()
-    #model._make_predict_function()
     with graph.as_default():
         #perform the prediction
-        #be.clear_session() 
-        
         out = model.predict(x)
-        #out = model._make_predict_function()
-        
-        #out = resnet_vgg.predict(x)
-        #print(out)
-        #print(np.argmax(out,axis=1))
-        #convert the response to a string
         response = np.array_str(np.argmax(out,axis=1))
-
         return response	
-
-    #in our computation graph
-
-    #return  render_template('index.html')
