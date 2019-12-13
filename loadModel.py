36b0cea77125abc0cf8a600112fcc6347758662c
jaymz95 <jamesmullarkey15@gmail.com>
2019-12-13 20:36:39 +0000

    Finnished Project

--- loadModel.py
+++ loadModel.py
@@ -27,22 +27,17 @@ from skimage.transform import resize
 #import tensorflow as tf
 
 def init(): 
-  json_file = open('C:/Users/james/Desktop/EmergingTechProject/model.json','r')
+  json_file = open('EmergingTechProject/model.json','r')
   loaded_model_json = json_file.read()
   json_file.close()
   loaded_model = model_from_json(loaded_model_json)
 	#load weights into new model
-  loaded_model.load_weights("C:/Users/james/Desktop/EmergingTechProject/model.h5")
+  loaded_model.load_weights("EmergingTechProject/model.h5")
 	#loaded_model.load_weights("model.h5")
   #loaded_model = load_model('model')
   print("Loaded Model from disk")
 
 	#compile and evaluate loaded model
   loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-	#loss,accuracy = model.evaluate(X_test,y_test)
-  print('loss:') 
-  #print(loss) 
-  print('accuracy:')
   graph = ops.get_default_graph()
-
   return loaded_model,graph
